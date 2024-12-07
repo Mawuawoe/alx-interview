7a0119100a1dc9abdf987e9e5e5fc82792c172b9
Mawuawoe <desmonddzakago@gmail.com>
2024-12-07 19:36:15 +0000

    task_1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,7 @@ Given a pile of coins of different values,
 determine the fewest number of coins needed to meet a given amount total.
 """
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
