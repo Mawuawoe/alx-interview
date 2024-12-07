8f45a070dd58e3aa618243d96a4b5e28be179a99
Mawuawoe <desmonddzakago@gmail.com>
2024-12-07 19:34:58 +0000

    task_1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""
+Given a pile of coins of different values,
+determine the fewest number of coins needed to meet a given amount total.
+"""
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    if isinstance(coins, list) and isinstance(total, int):
-        coins.sort(reverse=True)
-        count = 0
-        for coin in coins:
-            if coin <= total:
-                count += total // coin
-                total = total % coin
-        if total == 0:
-            return count
-        else:
-            return -1
+    if not coins or not all(isinstance(c, int) and c > 0 for c in coins):
+        return -1
+
+    # Sort coins in descending order
+    coins.sort(reverse=True)
+
+    count = 0
+    for coin in coins:
+        if coin <= total:
+            # Use as many coins as possible for this denomination
+            count += total // coin
+            total %= coin
+
+    return count if total == 0 else -1
